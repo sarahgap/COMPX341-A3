81b38980f3dffa7a1e30a3d27f4bfbd88606b9d1
sarahgap <102501524+sarahgap@users.noreply.github.com>
2022-05-27 17:15:14 +1200

    “COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -9,6 +9,8 @@ fi
 
 if npm run build; then
     echo "npm run build completed successfully"
+    git add .
+    git commit -m "“COMPX341-22A-A3 Commiting from CI/CD Pipeline"
 else
     echo "npm run build failed"
     exit
